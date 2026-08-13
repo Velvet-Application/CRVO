18215671692aedfbd7a2d59eab5308cd42ce2429
Velvet-Application <sh96hv64dj@privaterelay.appleid.com>
2026-08-13 10:52:43 +0200

    Afficher FTP dans toute l’interface CRVO

---
+++ app/ftp-terminology-patch.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { useEffect } from "react";
+
+function replaceLegacySftpLabels(root: ParentNode) {
+  const walker = document.createTreeWalker(root, NodeFilter.SHOW_TEXT);
+  const nodes: Text[] = [];
+  let current = walker.nextNode();
+  while (current) {
+    if (current.nodeValue?.includes("SFTP")) nodes.push(current as Text);
+    current = walker.nextNode();
+  }
+  for (const node of nodes) node.nodeValue = node.nodeValue?.replaceAll("SFTP", "FTP") ?? node.nodeValue;
+}
+
+export default function FtpTerminologyPatch() {
+  useEffect(() => {
+    replaceLegacySftpLabels(document.body);
+    const observer = new MutationObserver((mutations) => {
+      for (const mutation of mutations) {
+        for (const node of mutation.addedNodes) {
+          if (node.nodeType === Node.TEXT_NODE && node.nodeValue?.includes("SFTP")) {
+            node.nodeValue = node.nodeValue.replaceAll("SFTP", "FTP");
+          } else if (node instanceof HTMLElement) {
+            replaceLegacySftpLabels(node);
+          }
+        }
+      }
+    });
+    observer.observe(document.body, { childList: true, subtree: true });
+    return () => observer.disconnect();
+  }, []);
+  return null;
+}
