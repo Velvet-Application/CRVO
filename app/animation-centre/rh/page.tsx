ba71cd4a6a86a164f0294a324b2209c6b1878e6f
Velvet-Application <sh96hv64dj@privaterelay.appleid.com>
2026-08-17 16:49:57 +0200

    fix: route RH animation to stable effectif module

--- app/animation-centre/rh/page.tsx
+++ app/animation-centre/rh/page.tsx
@@ -1,5 +1,5 @@
-import EffectifClient from "../../data-rh/effectif/effectif-client";
+import { redirect } from "next/navigation";
 
 export default function AnimationCentreRhPage(){
-  return <EffectifClient context="animation"/>;
+  redirect("/data-rh/effectif?from=animation");
 }
