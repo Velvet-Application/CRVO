43059ff80b5c558bb2b8fd80d4d33709c63d3626
Velvet-Application <sh96hv64dj@privaterelay.appleid.com>
2026-08-18 17:19:42 +0200

    chore: remove desktop expertise-mobile route

--- app/developpement/expertise-mobile/page.tsx
+++
@@ -1,7 +0,0 @@
-import ExpertiseMobilePage from "../../expertise-mobile/page";
-
-export const dynamic = "force-dynamic";
-
-export default function DevelopmentExpertiseMobilePage() {
-  return <ExpertiseMobilePage />;
-}
