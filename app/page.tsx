b58298637f4512fc1642beebfece6243c05b0ab5
Velvet-Application <sh96hv64dj@privaterelay.appleid.com>
2026-08-20 07:50:57 +0200

    ui: expose site presence as second dashboard page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import DashboardPeriodFilter from "./dashboard-period-filter";
+import DashboardSectionNav from "./dashboard-section-nav";
 import HomeDashboard from "./home-dashboard";
 
 export default function Page(){
-  return <><HomeDashboard/><DashboardPeriodFilter/></>;
+  return <><DashboardSectionNav/><HomeDashboard/><DashboardPeriodFilter/></>;
 }
