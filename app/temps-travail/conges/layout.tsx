70409dd079fe4642bc305d6989337c21ad667f71
Velvet-Application <sh96hv64dj@privaterelay.appleid.com>
2026-08-20 10:59:19 +0200

    feat: gate CP day detail by selected activity

---
+++ app/temps-travail/conges/layout.tsx
@@ -0,0 +1,5 @@
+import LeaveDetailVisibility from "./leave-detail-visibility";
+
+export default function LeavePlanningLayout({ children }: { children: React.ReactNode }) {
+  return <><LeaveDetailVisibility />{children}</>;
+}
