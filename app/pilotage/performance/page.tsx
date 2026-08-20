1ece03a2d060ae1940aef2d675740cb543622a51
Velvet-Application <sh96hv64dj@privaterelay.appleid.com>
2026-08-20 15:33:15 +0200

    refactor: use global ToolBox hierarchy for pilotage navigation

--- app/pilotage/performance/page.tsx
+++ app/pilotage/performance/page.tsx
@@ -1,7 +1,6 @@
 import DashboardPeriodFilter from "../../dashboard-period-filter";
-import DashboardSectionNav from "../../dashboard-section-nav";
 import HomeDashboard from "../../home-dashboard";
 
 export default function PilotagePerformancePage(){
-  return <><DashboardSectionNav/><HomeDashboard/><DashboardPeriodFilter/></>;
+  return <><HomeDashboard/><DashboardPeriodFilter/></>;
 }
