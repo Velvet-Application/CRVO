8308c88b11a844fecb4f2bc1a3bfeac086337d93
Velvet-Application <sh96hv64dj@privaterelay.appleid.com>
2026-08-21 09:26:54 +0200

    feat: add dedicated Transphere dashboard route

---
+++ app/transphere/dashboard/page.tsx
@@ -0,0 +1,7 @@
+import TransphereDashboard from "../transphere-dashboard";
+
+export const dynamic = "force-dynamic";
+
+export default function TransphereDashboardPage(){
+  return <TransphereDashboard/>;
+}
