549ceb930bc6897981a6650a86074dfff1213905
Velvet-Application <sh96hv64dj@privaterelay.appleid.com>
2026-08-21 19:12:52 +0200

    feat: enable live notifications in network portal

--- app/qualite/client/[token]/page.tsx
+++ app/qualite/client/[token]/page.tsx
@@ -1,8 +1,9 @@
 import NetworkQualityPortal from "./network-quality-portal";
+import NetworkQualityLive from "./network-quality-live";
 
 export const dynamic = "force-dynamic";
 
 export default async function NetworkQualityPage({ params }: { params: Promise<{ token: string }> }) {
   const { token } = await params;
-  return <NetworkQualityPortal token={token} />;
+  return <><NetworkQualityPortal token={token} /><NetworkQualityLive token={token} /></>;
 }
